28a0474d8a5ace881f6753c90378fb6a725f8499
uncooldude <armedwarri.or.t.s@gmail.com>
2024-12-24 17:57:06 +0530

    Update AtoZ.jsx

--- src/pages/a2z/AtoZ.jsx
+++ src/pages/a2z/AtoZ.jsx
@@ -48,7 +48,7 @@ function AtoZ({ path }) {
     <div className="max-w-[1260px] mx-auto px-[15px] flex flex-col mt-[64px] max-md:mt-[50px]">
       <ul className="flex gap-x-2 mt-[50px] items-center w-fit max-[1200px]:hidden">
         <li className="flex gap-x-3 items-center">
-          <Link to="/" className="text-white hover:text-[#FFBADE] text-[17px]">
+          <Link to="/" className="text-white hover:text-[#3324f2] text-[17px]">
             Home
           </Link>
           <div className="dot mt-[1px] bg-white"></div>
@@ -56,7 +56,7 @@ function AtoZ({ path }) {
         <li className="font-light">A-Z List</li>
       </ul>
       <div className="flex flex-col gap-y-5 mt-6">
-        <h1 className="font-bold text-2xl text-[#ffbade] max-[478px]:text-[18px]">
+        <h1 className="font-bold text-2xl text-[#3324f2] max-[478px]:text-[18px]">
           Sort By Letters
         </h1>
         <div className="flex gap-x-[7px] flex-wrap justify-start gap-y-2 max-md:justify-start">
@@ -83,8 +83,8 @@ function AtoZ({ path }) {
               <Link
                 to={`/az-list/${linkPath}`}
                 key={index}
-                className={`text-md bg-[#373646] py-1 px-4 rounded-md font-bold hover:text-black hover:bg-[#FFBADE] hover:cursor-pointer transition-all ease-out ${
-                  isActive ? "text-black bg-[#FFBADE]" : ""
+                className={`text-md bg-[#373646] py-1 px-4 rounded-md font-bold hover:text-black hover:bg-[#3324f2] hover:cursor-pointer transition-all ease-out ${
+                  isActive ? "text-black bg-[#3324f2]" : ""
                 }`}
               >
                 {item}
